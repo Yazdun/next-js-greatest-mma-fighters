8d7ba95d8560571e583d99f94b6f0be600bddd53
Yazdun <yazduntube@gmail.com>
2022-07-14 17:39:25 +0430

    general : made progress

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,3 @@
 export * as anderson_silve_assets from '@assets/anderson_silva'
+export * as khabib_nurmagomedov_assets from '@assets/khabib-nurmagomedov'
+export * as daniel_cormier_assets from '@assets/daniel_cormier'
