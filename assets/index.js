1b9e97987cddabb8ca036a990b7cae35e67cf51a
Yazdun <yazduntube@gmail.com>
2022-07-14 19:07:41 +0430

    data : added a fighter

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
 export * as anderson_silve_assets from '@assets/anderson_silva'
 export * as khabib_nurmagomedov_assets from '@assets/khabib-nurmagomedov'
 export * as daniel_cormier_assets from '@assets/daniel_cormier'
+export * as stipe_miocic_assets from '@assets/stipe_miocic'
