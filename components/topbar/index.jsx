12d1c82698a45b50b2d3ff7457b86721c76c3675
Yazdun <yazduntube@gmail.com>
2022-07-15 20:29:44 +0430

    general : made progress

--- components/topbar/index.jsx
+++ components/topbar/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Search, Theme } from '@components/index'
+import { Theme } from '@components/index'
 import css from './styles.module.css'
-import Link from 'next/link'
 
 export const Topbar = () => {
   return (
