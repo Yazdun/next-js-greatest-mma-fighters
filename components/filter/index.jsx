490ae8ac395c497e75f5342322f091ffd97c853d
Yazdun <yazduntube@gmail.com>
2022-07-16 20:29:04 +0430

    general : made progress

--- components/filter/index.jsx
+++ components/filter/index.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import css from './styles.module.css'
+import { divisions } from '@data/index'
 
-export const Filter = () => {
-  return <div className={css.container}>Filter</div>
+export const Filter = ({}) => {
+  return (
+    <ul className={css.container}>
+      {divisions.map(division => {
+        const { name } = division
+        return (
+          <li key={name}>
+            <button className={css.btn}>{name}</button>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
