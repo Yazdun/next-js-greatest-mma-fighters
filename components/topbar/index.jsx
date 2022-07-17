1e71f28390d542cb86674980807d7c3f1cd7ef95
Yazdun <yazduntube@gmail.com>
2022-07-17 17:53:41 +0430

    general : made progress

--- components/topbar/index.jsx
+++ components/topbar/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Theme } from '@components/index'
 import css from './styles.module.css'
+import Link from 'next/link'
 
 export const Topbar = () => {
   return (
     <div className={css.container}>
       <nav className={css.nav}>
+        <Link href="/">
+          <a className={css.link}>MMA</a>
+        </Link>
         <Theme />
       </nav>
     </div>
