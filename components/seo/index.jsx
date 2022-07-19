defd07067640d031a2a55474252cadd2012cbbad
Yazdun <yazduntube@gmail.com>
2022-07-19 10:23:44 +0430

    general : made progress

--- components/seo/index.jsx
+++ components/seo/index.jsx
@@ -11,10 +11,25 @@ export const SEO = ({
       description={desc}
       additionalLinkTags={[{ rel: 'icon', href: '/favicon.ico' }]}
       openGraph={{
+        url: '',
         title: title,
         description: desc,
         locale: 'en_us',
+        images: [
+          {
+            url: './opengraph.png',
+            width: 1200,
+            height: 630,
+            alt: title,
+            type: 'image/png',
+          },
+        ],
         site_name: 'MMA Fighters',
+      }}
+      twitter={{
+        handle: '@Yazdun',
+        site: '@Yazdun',
+        cardType: 'summary_large_image',
       }}
     />
   )
